9077770d9f2bc228415b40671a910f96d30344a2
Ectras <40306539+Ectras@users.noreply.github.com>
2026-01-10 12:56:46 +0100

    Make delete_iterations work with already processed folders
    If the first iteration was already flattened and merged and one submits
    a second iteration, this opens a new PR where the script must delete the
    contents from the base folder before flattening the new iteration
    folder. This commit adds this functionality.

--- delete_iterations.py
+++ delete_iterations.py
@@ -1,4 +1,7 @@
-"""Deletes all but the latest iteration of each exercise and moves it up one folder (e.g. from foo/2/ to foo/)."""
+"""Deletes all but the latest iteration of each exercise and moves it up one folder (e.g. from foo/2/ to foo/).
+If there are already files in the exercise base folder AND also iteration folders (with numeric names), it is assumed
+that the files are from the unnesting of a previous iteration and hence it will delete these files and then move the files
+from the latest iteration folder up."""
 
 import argparse
 from pathlib import Path
@@ -7,6 +10,10 @@ import shutil
 
 def get_subdirs(dir: Path) -> list[Path]:
     return [subdir for subdir in dir.iterdir() if subdir.is_dir()]
+
+
+def get_iteration_subdirs(dir: Path) -> list[Path]:
+    return [subdir for subdir in get_subdirs(dir) if is_iteration_dir(subdir)]
 
 
 def is_iteration_dir(path: Path) -> bool:
@@ -22,18 +29,16 @@ def move_up_one_level(base_path: Path, relative_child_path: Path):
     tmp_base_path = base_path.rename(base_path.with_name(base_path.name + "__tmp"))
     new_child_path = tmp_base_path / relative_child_path
     new_child_path.rename(base_path)
-    tmp_base_path.rmdir()
+    shutil.rmtree(tmp_base_path)
 
 
 def delete_iteration_folders(track_path: Path):
     for exercise_dir in get_subdirs(track_path):
-        iteration_dirs = get_subdirs(exercise_dir)
+        iteration_dirs = get_iteration_subdirs(exercise_dir)
 
-        # Only consider the folder if it has subfolders and all subfolders have numeric names
-        is_unprocessed = len(iteration_dirs) > 0 and all(
-            is_iteration_dir(dir) for dir in iteration_dirs
-        )
-        if not is_unprocessed:
+        # Only consider the folder if there are iteration subdirs (i.e. folders with numeric names)
+        needs_processing = len(iteration_dirs) > 0
+        if not needs_processing:
             continue
 
         # Find the index of the latest iteration
@@ -49,7 +54,7 @@ def delete_iteration_folders(track_path: Path):
             print(f"Deleting {path}")
             shutil.rmtree(path)
 
-        # Move remaining iteration folder up one level
+        # Move remaining iteration folder up one level, deleting old contents of the base folder
         relative_path = latest_iteration_path.relative_to(exercise_dir)
         print(f"Moving {latest_iteration_path} to {exercise_dir}")
         move_up_one_level(exercise_dir, relative_path)
